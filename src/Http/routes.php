27877f09f07234766453d3c9b39a5ebd1b2c550b
Wiwat Srisattha <wiwat_srt@hotmail.com>
2017-03-18 23:09:27 +0700

    Added support role admin

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['namespace' => 'L5Starter\Core\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['web', 'auth']], function () {
+Route::group(['namespace' => 'L5Starter\Core\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['web', 'role:admin']], function () {
     Route::get('/', 'DashboardController@index');
     Route::get('dashboard', ['as' => 'admin.dashboard', 'uses' => 'DashboardController@index']);
 });
